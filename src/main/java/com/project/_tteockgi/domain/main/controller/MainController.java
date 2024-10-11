b5bb87ba4fd2307ad5928bf984347f1a03c54bc2
sik2 <bickwang123@gmail.com>
2024-10-11 13:41:20 +0900

    메인 게시글 출력

--- src/main/java/com/project/_tteockgi/domain/main/controller/MainController.java
+++ src/main/java/com/project/_tteockgi/domain/main/controller/MainController.java
@@ -1,14 +1,26 @@
 package com.project._tteockgi.domain.main.controller;
 
+import com.project._tteockgi.domain.article.entity.Article;
+import com.project._tteockgi.domain.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
 public class MainController{
+    private final ArticleService articleService;
+
+
     @GetMapping("/")
-    public String index () {
+    public String index (Model model) {
+        List<Article> articleList = articleService.getList();
+
+        model.addAttribute("articleList", articleList);
+
         return "main";
     }
 
