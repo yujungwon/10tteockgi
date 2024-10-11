d299f8688d24cb8bdb11d230723fcf112a28063b
Yoojungwon <a86522367@gmail.com>
2024-10-11 13:14:24 +0900

    서비스

--- src/main/java/com/project/_tteockgi/domain/auction/service/AuctionService.java
+++ src/main/java/com/project/_tteockgi/domain/auction/service/AuctionService.java
@@ -1,7 +1,20 @@
 package com.project._tteockgi.domain.auction.service;
 
+import com.project._tteockgi.domain.auction.entity.Auction;
+import com.project._tteockgi.domain.auction.repository.AuctionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class AuctionService {
+    private AuctionRepository auctionRepository;
+
+    public List<Auction> findAll() {
+        return auctionRepository.findAll();
+    }
+
+
 }
--- src/main/java/com/project/_tteockgi/domain/member/service/MemberService.java
+++ src/main/java/com/project/_tteockgi/domain/member/service/MemberService.java
@@ -1,7 +1,19 @@
 package com.project._tteockgi.domain.member.service;
 
+import com.project._tteockgi.domain.member.entity.Member;
+import com.project._tteockgi.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class MemberService {
+    private MemberRepository memberRepository;
+
+    public List<Member> findAll(){
+        return memberRepository.findAll();
+    }
+
 }
